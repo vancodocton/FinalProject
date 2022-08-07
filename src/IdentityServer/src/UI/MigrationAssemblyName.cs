62d158934592be56649777719a1ce89487b11c87
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2022-08-08 02:16:50 +0700

    Fix issue #11

--- src/IdentityServer/src/UI/MigrationAssemblyName.cs
+++ src/IdentityServer/src/UI/MigrationAssemblyName.cs
@@ -2,6 +2,6 @@
 {
     internal static class MigrationAssemblyName
     {
-        public const string SqlServer = "DuongTruong.IdentityServer.Infrastructure.SqlServer.Migrations";
+        public const string SqlServer = "DuongTruong.IdentityServer.Infrastructure.SqlServer";
     }
 }
