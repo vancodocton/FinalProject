849a8f941b6f638c65509f9450afa0b460cd0027
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2022-08-12 12:27:14 +0700

    Use method instead of magic string for MigrationAssemblyName.
    Resolve issue #24

---
+++ src/IdentityServer/src/Infrastructure.SqlServer/MigrationAssemblyName.cs
@@ -0,0 +1,9 @@
+﻿namespace DuongTruong.IdentityServer.Infrastructure
+{
+    public partial class MigrationAssemblyName
+    {
+        public static string SqlServer =>
+            typeof(MigrationAssemblyName).Assembly.GetName().Name
+            ?? throw new ArgumentNullException(nameof(SqlServer));
+    }
+}
