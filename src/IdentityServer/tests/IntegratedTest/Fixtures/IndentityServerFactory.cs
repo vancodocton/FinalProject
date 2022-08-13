e7727c350d286ced7dfd908f4ebfea795f6b1b21
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2022-08-13 23:17:46 +0700

    Add Test for IdentityServer Basic Endpoints

---
+++ src/IdentityServer/tests/IntegratedTest/Fixtures/IndentityServerFactory.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace DuongTruong.IdentityServer.IntegratedTest.Fixtures
+{
+    public class IndentityServerFactory : WebApplicationFactory<UI.Program>
+    {
+    }
+}
