408d5c3372abed942781e5640554a0809d42a697
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2022-08-25 19:27:24 +0700

    Create migrations for postgresql

---
+++ src/IdentityServer/src/Infrastructure.PostgreSql/Dependencies.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DuongTruong.IdentityServer.Infrastructure.PostgreSql
+{
+    public static class Dependencies
+    {
+        public static DbContextOptionsBuilder UseDefaultNpgsql(
+            this DbContextOptionsBuilder optionsBuilder,
+            string connectionString)
+        {
+            optionsBuilder.UseNpgsql(connectionString, options =>
+            {
+                options.MigrationsAssembly(Assembly.Name);
+            });
+
+            return optionsBuilder;
+        }
+    }
+}
