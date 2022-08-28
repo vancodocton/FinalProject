1219ce48fadd1fe6366489ac37100b38ac0b35cf
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2022-08-28 22:50:28 +0700

    Update UnitTest

--- src/IdentityServer/src/Infrastructure.PostgreSql/Dependencies.cs
+++ src/IdentityServer/src/Infrastructure.PostgreSql/Dependencies.cs
@@ -4,6 +4,18 @@ namespace DuongTruong.IdentityServer.Infrastructure.PostgreSql
 {
     public static class Dependencies
     {
+        public static DbContextOptionsBuilder<T> UseDefaultNpgsql<T>(
+            this DbContextOptionsBuilder<T> optionsBuilder,
+            string connectionString) where T : DbContext
+        {
+            optionsBuilder.UseNpgsql(connectionString, options =>
+            {
+                options.MigrationsAssembly(Assembly.Name);
+            });
+
+            return optionsBuilder;
+        }
+
         public static DbContextOptionsBuilder UseDefaultNpgsql(
             this DbContextOptionsBuilder optionsBuilder,
             string connectionString)
---
+++ src/IdentityServer/tests/UnitTest/Fixtures/TestDatabaseBase.cs
@@ -0,0 +1,35 @@
+﻿using DuongTruong.IdentityServer.Infrastructure.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace DuongTruong.IdentityServer.UnitTest.Fixtures
+{
+    public abstract class TestDatabaseBase<T> where T : DbContext
+    {
+        public abstract DbContextOptions<T> Options { get; protected set; }
+
+        public abstract T CreateContext();
+
+        protected virtual void EnsureDatabaseCreated(ref object _lock, ref bool isDatabaseInitialized)
+        {
+            lock (_lock)
+            {
+                if (!isDatabaseInitialized)
+                {
+                    using (var context = CreateContext())
+                    {
+                        context.Database.EnsureDeleted();
+
+                        if (context.Database.IsRelational())
+                            context.Database.Migrate();
+                        else
+                            context.Database.EnsureCreated();
+
+                        context.SaveChanges();
+                    }
+
+                    isDatabaseInitialized = true;
+                }
+            }
+        }
+    }
+}
