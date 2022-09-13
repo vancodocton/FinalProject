6f37ebdc8ec62d4b96f4bfd4697deaee237b42a5
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2022-09-13 19:48:31 +0700

    Add identityserver configuration for demoapp

--- src/IdentityServer/src/UI/Configurations/IdentityServerConfigurations.cs
+++ src/IdentityServer/src/UI/Configurations/IdentityServerConfigurations.cs
@@ -16,11 +16,50 @@ public static partial class IdentityServerConfigurations
         new List<ApiScope>()
         {
             new ApiScope(name: "api1", displayName: "MyAPI"),
+            new ApiScope(name: "demoapi", displayName: "Demo API"),
         };
 
     public static IEnumerable<Client> Clients =>
         new List<Client>()
         {
+            new Client
+            {
+                ClientId = "demoweb",
+                ClientSecrets = { new Secret("secret".Sha256()) },
+
+                AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+            
+                // where to redirect to after login
+                RedirectUris =
+                {                     
+                    "https://localhost:31403/signin-oidc",
+                    // add uri for testing.
+                    "https://oauth.pstmn.io/v1/browser-callback",
+                    "https://localhost:7292/swagger/oauth2-redirect.html",
+                },
+
+                AllowedCorsOrigins =
+                {
+                    // Allow origins for testing.
+                    "https://jwt.io",
+                    "https://localhost:7292",
+                },
+                
+                // where to redirect to after logout
+                PostLogoutRedirectUris =
+                {
+                    "https://localhost:31403/signout-callback-oidc"
+                },
+
+                AllowOfflineAccess = true,
+
+                AllowedScopes = new List<string>
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    "demoapi",
+                },
+            },
             // interactive ASP.NET Core Web App
             new Client
             {
