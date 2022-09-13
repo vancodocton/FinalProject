89a5a767fb7f7b62113691734f7ecf478f843b9a
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2022-09-13 22:17:42 +0700

    Add OpenIdConnect to Demo WebApp

--- src/DemoApp/src/Web/Program.cs
+++ src/DemoApp/src/Web/Program.cs
@@ -1,8 +1,32 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+})
+    .AddCookie()
+    .AddOpenIdConnect(options =>
+    {
+        var config = builder.Configuration.GetRequiredSection(OpenIdConnectDefaults.AuthenticationScheme);
+        // Token issuer - IdentityServer
+        options.Authority = config.GetValue<string>("Issuer");
+        options.ClientId = config.GetValue<string>("ClientId");
+        options.ClientSecret = config.GetValue<string>("ClientSecret");
+        options.ResponseType = OpenIdConnectResponseType.Code;
 
+        options.SaveTokens = true;
+
+        // implict add openid profile scope // skip adding
+        options.Scope.Add("demoapi");
+        options.Scope.Add(OpenIdConnectScope.OfflineAccess);
+    });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,8 +42,10 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapRazorPages()
+    .RequireAuthorization();
 
 app.Run();
